66730442b0f28bf8eb22ae55a95a9cf5bef9f3a1
David Rossig <damignol44@gmail.com>
2021-10-04 10:41:09 +0200

    Ex1 Commit

---
+++ ex01.ts
@@ -0,0 +1,16 @@
+function lowerCase(str = ''): string{
+    return str.toLowerCase()
+}
+
+console.log (lowerCase('CHOPPER'));
+console.log (lowerCase('Tony Stark'));
+
+/*console.log('Hello World')
+/* npx ts-node ex01.ts pour executer le script */
+/* Node LTS si bug :
+> npm i typescript @types/node ts-node
+> npx ts-node ex0.ts
+Node Current
+> npm i ts-node
+>npx e01.ts
+*/
